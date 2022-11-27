37e0a51634a4de5367508bc24868d13d649e9f5a
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2022-11-27 02:45:17 +0100

    last comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { TaskGroupListComponent } from './view/task-groups-list/task-group-list.component';
+import { TasksListComponent } from './view/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    TaskGroupListComponent,
+    TasksListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view/task-groups-list/task-group-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-group-list',
+  templateUrl: './task-group-list.component.html',
+  styleUrls: ['./task-group-list.component.sass']
+})
+export class TaskGroupListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
